0e01e72300a40c2655d3387f48d4557b359dfa8c
skiy <skiy@DESKTOP-9VCVHDO>
2023-06-19 22:03:19 +0200

    Add project files.

---
+++ ConsoleEngine/Program.cs
@@ -0,0 +1,52 @@
+﻿namespace ConsoleEngine;
+
+public class Program
+{
+    private const int ScreenWidth = 100;
+    private const int ScreenHeight = 75;
+
+#pragma warning disable CS8618 
+    private static Screen _screen;
+#pragma warning restore CS8618 
+
+    private static void Main()
+    {
+        _screen = new(ScreenWidth, ScreenHeight);
+
+        DrawLine(1, 1, 6, 3);
+        _screen.Update();
+
+        //while (true)
+        //{
+        //    if (Console.KeyAvailable)
+        //    {
+        //        char charToFill = Console.ReadKey().KeyChar;
+        //        _screen.Fill(charToFill);
+        //    }
+
+        //    _screen.Update();
+        //}
+    }
+
+    private static void DrawLine(int x1, int y1, int x2, int y2)
+    {
+        float error = 0;
+        float slope = (y2 - y1) / (float)(x2 - x1);
+        
+        _screen.SetPixel(x1, y1);
+
+        int y = y1;
+        for (int x = x1 + 1; x <= x2; x++)
+        {
+            error += slope;
+
+            if (error > 0.5f)
+            {
+                y++;
+                error--;
+            }
+
+            _screen.SetPixel(x, y);
+        }
+    }
+}
---
+++ ConsoleEngine/Screen.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleEngine;
+
+internal class Screen
+{
+    public int Width { get; }
+    public int Height { get; }
+    public char[] Buffer { get; private set; }
+
+    public Screen(int width, int height)
+    {
+        Width = width;
+        Height = height;
+        Buffer = new char[Width * Height];
+        Console.SetWindowSize(Width, Height);
+        Console.SetBufferSize(Width, Height);
+        Console.CursorVisible = false;
+        Clear();
+    }
+
+    public void SetPixel(int x, int y, char c = '█')
+    {
+        Buffer[(Height - y) * Width + x] = c;
+    }
+
+    public void ClearPixel(int x, int y)
+    {
+        Buffer[(Height - y) * Width + x] = ' ';
+    }
+
+    public void Fill(char c = '█')
+    {
+        Array.Fill(Buffer, c);
+    }
+
+    public void Clear()
+    {
+        Array.Fill(Buffer, ' ');
+    }
+
+    public void Update()
+    {
+        Console.SetCursorPosition(0, 0);
+        Console.Write(Buffer);
+    }
+}
